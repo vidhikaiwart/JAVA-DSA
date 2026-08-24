2366ac1853d95a5078d230055d19e7858630aadd
vidhikaiwart <vidhikaiwart@gmail.com>
2026-08-24 21:54:57 +0530

    Add basic Java example

---
+++ 1_Basic.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main
+{
+    //PRIMITIVE DATATYPE
+    public static void main(String[] args){
+    
+    
+    int rollNum=32;
+    char section = 'A';
+    String name = "vidhi";
+    float marks = 85.34f;
+    double largeDec = 3456732.4356;
+    long largeInt = 345345555555555556L;
+    boolean check = false;
+        
+        
+    System.out.println( "Student rollNum : "+ rollNum);
+    
+    System.out.println("Student section:  " + section );
+    System.out.println("Student name : "+ name);
+    System.out.println("Student marks: " + marks);
+    System.out.println("Student double " + largeInt );
+    System.out.println("Student long  "+largeDec);
+    System.out.println("Student bool  "+ check);
+    
+    
+    
+    //INPUT/OUTPUT
+// 	public static void main(String[] args) {
+// 	   Scanner input = new Scanner (System.in);
+	   //int rollNum = input.nextInt();
+	   //System.out.println("Your rollNum is : "+rollNum);
+	   
+	   //String name = input.nextLine();
+	   //System.out.println("enter your name  : "+ name);
+	   
+	   //float marks = input.nextFloat();
+	   //System.out.println("enter your marks: "+ marks);
+	   
+
+	
+	//TYPECASTING
+// 	public static void main(String[] args) {
+// 	   Scanner input = new Scanner (System.in);
+	   
+	   //this output alway in float
+	   //float num1 = input.nextFloat();
+	   //float num2 = input.nextFloat();
+	   //System.out.println(num1 + num2);
+	   
+	   //this only print integer part 
+	   //int num = (int)(2343.3454f);
+	   //System.out.println(num);
+	   
+	   
+	   
+	}
+}
